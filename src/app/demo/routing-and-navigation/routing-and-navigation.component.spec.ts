db5a1d97c8501ffa2f2a80bdc440899c7a91d4e3
tumit <tumit@live.com>
2022-05-31 07:30:56 +0700

    test: fix testing

--- src/app/demo/routing-and-navigation/routing-and-navigation.component.spec.ts
+++ src/app/demo/routing-and-navigation/routing-and-navigation.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { RoutingAndNavigationComponent } from './routing-and-navigation.component';
 
 describe('RoutingAndNavigationComponent', () => {
@@ -8,6 +8,7 @@ describe('RoutingAndNavigationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [RoutingAndNavigationComponent],
     }).compileComponents();
   });
