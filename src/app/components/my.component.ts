067064ec4ab5bf9db636bbfcea65734a30ddb56b
tumit <tumit@live.com>
2022-05-31 15:24:54 +0700

    refactor: fix format

--- src/app/components/my.component.ts
+++ src/app/components/my.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my',
-  template: ` <h1>This is my component</h1> `,
+  template: `
+    <h1>This is my component</h1>
+  `,
 })
 export class MyComponent {}
